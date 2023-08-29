e8ca0f0f36749775d29516152d93845d7d46a8b6
Jens Shumway <jensshum@gmail.com>
2023-08-29 15:21:22 -0600

    Working on AB private Investor Corp

--- 10-Q_pull.py
+++ 10-Q_pull.py
@@ -40,158 +40,119 @@ companyData['cik_str'] = companyData['cik_str'].astype(
                            str).str.zfill(10)
 
 # review data
-# print(companyData[:1])
 
-company_data_dict = companyData.set_index('ticker')['cik_str'].to_dict()
+company_data_dict = companyData.set_index('cik_str')['ticker'].to_dict()
 
-# print(company_data_dict)
-# with open("file.txt",'w') as f:
-#     f.write(str(company_data_dict))
-# sys.exit()
 
-cik = company_data_dict['ARCC']
+cik_tickers = ["1634452", "1578620","1278752","1287750","1633858","1655050",	"1379785","1326003","1370755","1736035","1490927","17313","1571329","1534254","1578348","1617896","1633336","878932", "1513363", "1495584","1701724","1501729",
+"1637417","1525759","1579412","1422183","1509892","1143513","1321741", "1572694","1683074","1674760","1476765","1715268","1627515","1675033","1509470","1618697","1661306","1559909", "1280784","1535778","1487428","1550913","1396440",
+"1490349","1512931","1742313","1099941","1496099","1588272","1414932","1577791", "1744179", "1487918","1297704", "1655888", "1655887", "1747777", "1259429", "1626899", "1383414","1504619", "1372807", "845385", "1287032","81955", "1653384", 
+"1743415", "1523526", "1614173", "1418076",	"1508171", "1551901", "1702510", "1544206", "1603480", "1715933", "1577134", "1464963","1521945","1508655","1580345", "1717310", "1557424","1642862", "1552198"]
 
-# print(cik)
+cik_tickers = [value.zfill(10) for value in cik_tickers]
 
 # # get company specific filing metadata
-filingMetadata = requests.get(
-    f'https://data.sec.gov/submissions/CIK{cik}.json',
-    headers=headers
-    )
 
-# review json 
-# print(filingMetadata.json().keys())
-filingMetadata.json()['filings']
-filingMetadata.json()['filings'].keys()
-filingMetadata.json()['filings']['recent']
-filingMetadata.json()['filings']['recent'].keys()
+html_links = []
+for cik in cik_tickers:
+    filingMetadata = requests.get(
+        f'https://data.sec.gov/submissions/CIK{cik}.json',
+        headers=headers
+        )
 
-# dictionary to dataframe
-allForms = pd.DataFrame.from_dict(
-             filingMetadata.json()['filings']['recent']
-             )
+    # review json 
+    # print(filingMetadata.json().keys())
+    # filingMetadata.json()['filings']
+    # filingMetadata.json()['filings'].keys()
+    # filingMetadata.json()['filings']['recent']
+    # filingMetadata.json()['filings']['recent'].keys()
 
-# review columns
-allForms.columns
-allForms[['accessionNumber', 'reportDate', 'form']].head(80)
+    # dictionary to dataframe
+    allForms = pd.DataFrame.from_dict(
+                filingMetadata.json()['filings']['recent'])
 
-# 10-Q metadata
-allForms.iloc[11]
-thing = allForms[allForms['form'] == '10-Q'].reset_index(drop = True)
+    # review columns
+    allForms.columns
+    allForms[['accessionNumber', 'reportDate', 'form']].head(100)
 
-accession_number = []
-html = thing["primaryDocument"]
-for number in thing["accessionNumber"]:
-    accession_number.append(number.replace("-",""))
+    # 10-Q metadata
+    allForms.iloc[11]
+    forms = allForms[allForms['form'] == '10-Q'].reset_index(drop = True)
 
-url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number[0]}/{html[0]}"
+    accession_numbers = forms.loc[forms["form"] == "10-Q", "accessionNumber"].tolist()
+    html_caps = forms.loc[forms["form"] == "10-Q", "primaryDocument"].tolist()
 
-response = requests.get(url, headers=headers)
-html_content = response.text
+    # with open("html.txt", "w") as f:
+    #     f.write(str(html))
 
-# with open("file.html", 'w') as f:
-#     f.write(str(html_content))
-# print(response)
+    for html, num in zip(html_caps, accession_numbers):
+        num = num.replace("-","")
+        url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{num}/{html}"
 
-l = html_content.split("CONSOLIDATED SCHEDULE OF INVESTMENTS</span>")
+        response = requests.get(url, headers=headers)
+        html_content = response.text
+
+        # print(html_content)
+
+        ## WORKING FOR AB Private Investor Corp
+        investment_tables = html_content.lower().split("schedule of investments")
+
+        if len(investment_tables) != 1:
+            continue
+            
+        tables = []
+        cols = []
+        df = pd.DataFrame()
+        for i,table_text in enumerate(investment_tables[1:]):
+
+            values = []
+            soup = BeautifulSoup(table_text, 'html.parser')
+            table = soup.find('table')
+            row_num = 0
+
+            for row in table.find_all('tr'):
+                columns = row.find_all('td')
+                row_data = {}
+                for j, col in enumerate(columns):
+                    cell_text = col.get_text(strip=True)
+                    row_data[f'col{j}'] = cell_text
+                values.append(row_data)
+                
+            df_to_append = pd.DataFrame(values)
+            if i == 0:
+                cols = [str(value).strip() for value in df_to_append.iloc[1]]
+
+            df_to_append = df_to_append[2:]
+            df = pd.concat([df, df_to_append], ignore_index=True)
 
 
-tables = []
-cols = []
-df = pd.DataFrame()
-for i,table_text in enumerate(l[1:]):
 
-    values = []
-    soup = BeautifulSoup(table_text, 'html.parser')
-    table = soup.find('table')
-    row_num = 0
+    # cols_to_fill = ["0","2"]
+    # for col in cols_to_fill:
+    #     col0_data = df[f"col{col}"]
+    #     col_data = col0_data[0]
+    #     temp_data = col_data
+    #     for index, data in enumerate(col0_data):
+    #         if str(col_data) != "nan":
+    #             temp_data = col_data
+    #         col_data = data
+    #         if str(data) == "nan":
+    #             df.at[index, f"col{col}"] = temp_data
 
-    for row in table.find_all('tr'):
-        columns = row.find_all('td')
-        row_data = {}
-        for j, col in enumerate(columns):
-            cell_text = col.get_text(strip=True)
-            if cell_text == "":
-                continue
-            # column_index = len(row_data) + 1
-            row_data[f'col{j}'] = cell_text
-        values.append(row_data)\
-        
-    df_to_append = pd.DataFrame(values)
-    if i == 0:
-        cols = [str(value).strip() for value in df_to_append.iloc[1]]
+        try:
+            df.columns = cols
+            col_mapping = {"Company (1)": "Company Name", "Investment": "Security", "Par / Units": "Principal", "Amortized Cost(3)(4)": "Amortized Cost"}
+            df = df.rename(columns = col_mapping)
+        except Exception:
+            df.to_csv("testfile.csv", index=False)
+            break
 
-    df_to_append = df_to_append[2:]
-    df = pd.concat([df, df_to_append], ignore_index=True)
-
-df.to_csv("testfile.csv",index=False)
-print(cols)
-
-df.columns = cols
-col_mapping = {"Company (1)": "Company Name", "Investment": "Security", "Par / Units": "Principal", "Amortized Cost(3)(4)": "Amortized Cost"}
-df = df.rename(columns = col_mapping)
 
     # Specific to Blue Owl Capital
     # df['Reference Rate'] = df['Interest'].apply(lambda x: x.split(' ')[0])
     # df['Spread'] = df['Interest'].apply(lambda x: x.split(' ')[1])
     # del df['Interest']
 
-df.to_csv("testfile.csv", index=False)
 
-    #         column_index = len(row_data) + 1
-    #         row_data[f'col{column_index}'] = cell_text
-    #     values.append(row_data)
-    # tables.append(values)  
-            # values = [{"col1":"val1","col2":"val1"},{"col1" :"val2","col2":"val2"}]
-            # >>> df = pd.DataFrame(values)
-            # >>> df
-            # col1  col2
-            # 0  val1  val1
-            # 1  val2  val2
-        
-    # break
-
-    
-            
-            
-
-
-
-
-# values = [
-#     {"col1": value1, "col2", value2, etc.} # each td -> "colx": valuex
-# ]
-
-# for element in soup.find_all(attrs={'style': True}):
-#     element.attrs['style'] = ''
-# tables = soup.find_all('table')
-# new_soup = BeautifulSoup(tables[9], 'html.parser')
-# ix_elements = soup.find_all('ix:nonfraction')
-
-# xbrl_content = '\n'.join(str(element) for element in ix_elements)
-
-# Create an IXBRL object to parse the XBRL content
-
-
-# # Extract data from parsed XBRL content
-# data = []
-# for element in xbrl.data:
-#     if element.name == "us-gaap:RealizedAndUnrealizedGainLossInvestmentDerivativeAndForeignCurrencyTransactionPriceChangeOperatingAfterTax":
-#         data.append(element.value)
-
- 
-# with open("testie.txt", "w") as f:
-#     f.write(str(data))
-
-# xbrl = IXBRL("testtable.xml")
-
-# df = pd.read_html(tables[8])
-
-
-# df.to_csv("table.csv")
-
-
-
-# thing.to_csv("blingoblongo.csv")
 
 # https://www.sec.gov/Archives/edgar/data/1287750/000128775023000036/arcc-20230630.htm
