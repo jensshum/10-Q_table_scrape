1340de7e7c39ed2cd1d357fa5afa5e2205c25c61
Jens Shumway <jensshum@gmail.com>
2023-09-09 11:26:59 -0600

    Recent Changes

--- 10-Q_pull.py
+++ 10-Q_pull.py
@@ -14,9 +14,59 @@ from bs4 import BeautifulSoup
 import os
 import openai
 
+from format_AB_private_investor import format_AB_private_investor
+from format_arcc import format_arcc
+
 LLM = 'gpt-4'
 # create request header
 headers = {'User-Agent': "email@address.com"}
+
+# FUNCTIONS ---------------------------------------------------------
+
+def get_accession_nums_and_html(cik):
+    """
+    Gets accession number and html cap to be used in accessing the corresponding 10-Q url.
+    """
+    filingMetadata = requests.get(
+        f'https://data.sec.gov/submissions/CIK{cik}.json',
+        headers=headers
+        )
+    # dictionary to dataframe
+    allForms = pd.DataFrame.from_dict(
+                filingMetadata.json()['filings']['recent'])
+
+    # review columns
+    allForms.columns
+    allForms[['accessionNumber', 'reportDate', 'form']].head(100)
+
+    # 10-Q metadata
+    allForms.iloc[11]
+    forms = allForms[allForms['form'] == '10-Q'].reset_index(drop = True)
+
+    accession_numbers = forms.loc[forms["form"] == "10-Q", "accessionNumber"].tolist()
+    html_caps = forms.loc[forms["form"] == "10-Q", "primaryDocument"].tolist()
+    return accession_numbers, html_caps
+
+def get_url(cik,num,html):
+    """
+    Gets the url to be used for extracting the html from the 10-Q.
+    """
+    num = num.replace("-","")
+    url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{num}/{html}"
+    return url
+
+def get_html(url):
+    """
+    Gets the html content to be extracted from the 10-Q url.
+    """
+    response = requests.get(url, headers=headers)
+    html_content = response.text
+    return html_content
+
+
+
+# MAIN --------------------------------------------------------------
+
 
 # get all companies data
 companyTickers = requests.get(
@@ -42,7 +92,6 @@ companyData['cik_str'] = companyData['cik_str'].astype(
                            str).str.zfill(10)
 
 # review data
-
 company_data_dict = companyData.set_index('cik_str')['ticker'].to_dict()
 
 
@@ -57,45 +106,23 @@ cik_tickers = [value.zfill(10) for value in cik_tickers]
 
 html_links = []
 for cik in cik_tickers:
-    filingMetadata = requests.get(
-        f'https://data.sec.gov/submissions/CIK{cik}.json',
-        headers=headers
-        )
-
-    # dictionary to dataframe
-    allForms = pd.DataFrame.from_dict(
-                filingMetadata.json()['filings']['recent'])
-
-    # review columns
-    allForms.columns
-    allForms[['accessionNumber', 'reportDate', 'form']].head(100)
-
-    # 10-Q metadata
-    allForms.iloc[11]
-    forms = allForms[allForms['form'] == '10-Q'].reset_index(drop = True)
-
-    accession_numbers = forms.loc[forms["form"] == "10-Q", "accessionNumber"].tolist()
-    html_caps = forms.loc[forms["form"] == "10-Q", "primaryDocument"].tolist()
+    
+    accession_numbers, html_caps = get_accession_nums_and_html(cik)
 
     k = 1
     for html, num in zip(html_caps, accession_numbers):
         
-        num = num.replace("-","")
-        url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{num}/{html}"
+        url = get_url(cik,num,html)
         print(url)
 
-        response = requests.get(url, headers=headers)
-        html_content = response.text
+        html_content = get_html(url)
 
-        ## WORKING FOR AB Private Investor Corp -- 1634452 cik
-        # if "1634452" in cik:
+        # Checks html_content for the correct tables.
         investment_tables = html_content.lower().split("schedule of investments")
-        if len(investment_tables) == 1:
+        if len(investment_tables) < 2:
             print("No schedule info")
             continue
-
-        # print(f"10-Q file num {k} of", len(accession_numbers), f"for {cik}")
-
+        
         tables = []
         df = pd.DataFrame()
         for i, table_text in enumerate(investment_tables[1:]):
@@ -104,85 +131,49 @@ for cik in cik_tickers:
             table = soup.find('table')
             if table == None:
                 continue
-
-            row_num = 0
-
             for row in table.find_all('tr'):
                 columns = row.find_all('td')
                 row_data = {}
                 for j, col in enumerate(columns):
                     cell_text = col.get_text(strip=True)
-                    if "?" in cell_text: 
-                        continue
                     row_data[f'col{j}'] = cell_text
                 values.append(row_data)
                 
             df_to_append = pd.DataFrame(values)
-            # if i == 2:
-            #     cols = [str(value).strip() for value in df_to_append.iloc[1]]
-            #     print(cols)
-
             df_to_append = df_to_append[2:]
-            if len(df_to_append.columns) > 21:
-                continue
+            if "1287750" in cik:
+                print(len(df_to_append.columns))
+                if len(df_to_append.columns) < 20:
+                    continue
+            elif "1634452" in cik:
+                if len(df_to_append.columns) > 24:
+                    continue
+
             df = pd.concat([df, df_to_append], ignore_index=True)
 
-            ## For ARCC
-            # cols_to_fill = ["0","2"]
-            # for col in cols_to_fill:
-            #     col0_data = df[f"col{col}"]
-            #     col_data = col0_data[0]
-            #     temp_data = col_data
-            #     for index, data in enumerate(col0_data):
-            #         if str(col_data) != "nan":
-            #             temp_data = col_data
-            #         col_data = data
-            #         if str(data) == "nan":
-            #             df.at[index, f"col{col}"] = temp_data
+        print(cik)
+        if "1287750" not in cik:
+            break
+        if "1634452" in cik:
+            break
+            df = format_AB_private_investor(df)
+        elif "1287750" in cik:
+            df = format_arcc(df)
 
-            # try:
-            # col_mapping = {"Company (1)": "Company Name", "Investment": "Security", "Par / Units": "Principal", "Amortized Cost(3)(4)": "Amortized Cost"}
-            #     df = df.rename(columns = col_mapping)
-            # except Exception:
-
-        # df.columns = cols
-        corrected_cols = ["Company Name","Industry","Security","Reference Rate","Spread","Interest Rate","Maturity Date","Fair", "Value", "Principal","Amortized Cost","Investment Date"]
-
-        col_mapping = {"col0": "Company Name", "col2" : "Industry", "col4": "Security", "col6": "Interest", "col8": "Maturity Date", "col11": "Principal", "col15": "Amortized Cost", "col19": "Fair Value" }
-        
-        df = df[6:]
-        df = df.drop(["col1","col3","col5","col7","col9","col10","col12","col13","col14","col16","col17","col18","col20"],axis=1)
-        
-        df = df.rename(columns=col_mapping)
-
-        df["Reference Rate"] = df['Interest'].apply(lambda x: str(x).replace("—","").split(' ')[0])
-        # df["Spread"] = df["Interest"].apply(lambda x: " ".join(str(x).split(" ")[1:]) if 3 > len(str(x).split(' ')) > 6 else "nan")
-        df["Spread"] = df["Interest"].apply(lambda x: " ".join(str(x).replace("—","").replace("(","").split(" ")[1:4]) if 2 < len(str(x).split(" ")) <= 6 else " ".join(str(x).split(" ")[1:6]))
-        df["Interest Rate"] = df["Interest"].apply(lambda x: " ".join(str(x).replace("—","").replace("(","").split(" ")[4:]) if 2 < len(str(x).split(" ")) <= 6 else " ".join(str(x).split(" ")[6:]))
-        # df[""]
-        df = df.drop("Interest", axis=1)
-        # df = df.drop(df.loc("").
-        df = df[df.iloc[:, 0] != '']
-
-        order_of_columns = ["Company Name","Industry","Security","Reference Rate","Spread","Interest Rate","Maturity Date","Fair Value","Principal","Amortized Cost"]
-        # print(len(df['']))
-
-        df = df[order_of_columns]
-        try:
-            os.mkdir(f"CIK_{cik}")
-        except Exception:
-            pass
-        df.to_csv(f"CIK_{cik}/CIK_{cik}_filenum_{k}_of_{len(accession_numbers)}_test.csv", index=False)
-        k += 1
-        # break
-    sys.exit()
+            try:
+                os.mkdir(f"CIK_{cik}")
+            except Exception:
+                pass
+            df.to_csv(f"CIK_{cik}/CIK_{cik}_filenum_{k}_of_{len(accession_numbers)}_test.csv", index=False)
+            k += 1
+            sys.exit()
+        break
 
     # Specific to Blue Owl Capital
     # df['Reference Rate'] = df['Interest'].apply(lambda x: x.split(' ')[0])
     # df['Spread'] = df['Interest'].apply(lambda x: x.split(' ')[1])
     # df['Spread'] = df['Interest'].apply(lambda x: x.split(' ')[1])
     # del df['Interest']
-
 
 
 # https://www.sec.gov/Archives/edgar/data/1287750/000128775023000036/arcc-20230630.htm
--- format_arcc.py
+++ format_arcc.py
@@ -1,18 +1,27 @@
 #cik = 1287750
 
-def format_arcc():
 
+def fill_columns(df,col):
+    """
+    Takes a dataframe and column index as arguments, fills any hierarchical data with the previous input
+    """
+    print("zilco")
+    col0_data = df[f"col{col}"]
+    col_data = col0_data[0]
+    temp_data = col_data
+    for index, data in enumerate(col0_data):
+        if str(col_data) != "nan":
+            temp_data = col_data
+        col_data = data
+        if str(data) == "nan":
+            df.at[index, f"col{col}"] = temp_data
+    
 
-    pass
-    ## For ARCC
-    # cols_to_fill = ["0","2"]
-    # for col in cols_to_fill:
-    #     col0_data = df[f"col{col}"]
-    #     col_data = col0_data[0]
-    #     temp_data = col_data
-    #     for index, data in enumerate(col0_data):
-    #         if str(col_data) != "nan":
-    #             temp_data = col_data
-    #         col_data = data
-    #         if str(data) == "nan":
-    #             df.at[index, f"col{col}"] = temp_data
+def format_arcc(df):
+
+    # For ARCC
+    cols_to_fill = ["0","2"]
+    for col in cols_to_fill:
+        fill_columns(df,col)
+    
+    return df
